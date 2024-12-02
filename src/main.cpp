eaf5fbd0cce53ee68758b468d6057c72ec809195
MT <dev.tahirulbari2002@hotmail.com>
2024-12-02 17:55:24 +0500

    batmanAdd .gitattributes, README.md, and LICENSE.txt.

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+
+    std::cout<<"This is Project which consist of 20 project done using 'SFML'.\nIt includes project from very beginner to advance level."<<std::endl;
+    return 0;
+}
